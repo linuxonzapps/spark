fd46321d7031d849c4b489310d2dfa79e4932cf0
rposts <rposts>
2025-10-21 18:33:11 +0000

    Fix package name

--- src/build.sh
+++ src/build.sh
@@ -6,5 +6,5 @@ trap 0 1 2 ERR
 # Ensure sudo is installed
 apt-get update && apt-get install sudo -y
 bash /tmp/linux-on-ibm-z-scripts/ApacheSpark/${version}/build_spark.sh -y
-tar cvfz spark-shell-${version}-linux-s390x.tar.gz -C $PWD/spark/bin spark-shell
+tar cvfz spark-${version}-linux-s390x.tar.gz -C $PWD/spark/bin spark-shell
 exit 0
