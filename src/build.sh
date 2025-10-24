65db1004d3a6b91edfd10050a059901ffcf5d4fd
rposts <rposts>
2025-10-24 17:56:52 +0000

    Add distribution to archive

--- src/build.sh
+++ src/build.sh
@@ -6,5 +6,6 @@ trap 0 1 2 ERR
 # Ensure sudo is installed
 apt-get update && apt-get install sudo -y
 bash /tmp/linux-on-ibm-z-scripts/ApacheSpark/${version}/build_spark.sh -y
-tar cvfz spark-${version}-linux-s390x.tar.gz -C $PWD/spark/bin spark-shell
+source ./setenv.sh && ./spark/dev/make-distribution.sh
+tar cvfz spark-${version}-linux-s390x.tar.gz -C $PWD/spark/ dist
 exit 0
